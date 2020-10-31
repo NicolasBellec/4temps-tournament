6ad7e4c7f99c9349a1216349c3271a8e2512a4e1
Nicolas Bellec <nicolas.bellec@ens-rennes.fr>
2020-10-31 18:14:48 +0100

    Config jest

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,3 @@
-// @flow
-// import type { Config } from '@jest/types';
-//
-// const config: Config.InitialOptions = {
-//   verbose: true,
-//   coverageReporters: ["text", "html", "lcov"],
-//   testEnvironment: "node"
-// };
-
-// export default config;
-
 module.exports = {
     // verbose: true,
     coverageReporters: ["text", "html", "lcov"],
