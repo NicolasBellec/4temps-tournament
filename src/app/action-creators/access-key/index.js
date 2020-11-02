0bb6a252022a0be783ea01889429a08d6d2f7263
Nicolas Bellec <nicolas.bellec@ens-rennes.fr>
2020-11-02 19:31:40 +0100

    Correct mispelled get_access_keys

--- src/app/action-creators/access-key/index.js
+++ src/app/action-creators/access-key/index.js
@@ -1,9 +1,9 @@
 // @flow
-import { GET_ACCESS_KEY } from '../action-types'
+import { GET_ACCESS_KEYS } from '../action-types'
 
 export function getAccessKeysAction(tournamentId: string): GetAccessKeyAction {
   return {
-    type: GET_ACCESS_KEY,
+    type: GET_ACCESS_KEYS,
     promise: getAccessKeysForTournament(tournamentId),
   }
 }
